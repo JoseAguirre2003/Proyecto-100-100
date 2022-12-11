6c0f30ff1b029bea0e9b8422ff1acc7345558df8
José Aguirre <josealfredo02aguirre@gmial.com>
2022-12-11 18:00:23 -0400

    actualizacion, Termando V1 proyecto

--- ProyectoTest.c
+++ ProyectoTest.c
@@ -17,7 +17,8 @@ void menuConjunto();
 void menuComplementos();
 void complementoDeConj(int *conj, int size);
 void complementConjRespectoA(int *conj1, int *conj2, int size1, int size2);
-
+void diferenciaSimetrica(int *conj1, int *conj2, int size1, int size2);
+void complemetAuBuC();
 
 int main(){
 
@@ -36,61 +37,50 @@ int main(){
 
     menuPrincipal();
 
-    system("pause");
-
     return 0;
 }
 
 void rellenarUniverso(){
-
     for(int i = 0; i < tamano; i++){
         
         printf("Ingrese el valor %i: ", i+1);
         scanf("%i", &universo[i]);
         fflush(stdin);
-        
     }
-
 }
 
 void rellenarConjuntos(){
-
     int a = 0, b = 0, c = 0;
-
     for (int i = 0; i < tamano; i++){
-
         if(universo[i] % 2 == 0){
             conjA[a] = universo[i];
             a++;
             sizeA++;
         }
-
         if(universo[i] % 3 == 0){
             conjB[b] = universo[i];
             b++;
             sizeB++;
         }
-
         if(universo[i] % 4 == 0){
             conjC[c] = universo[i];
             c++;
             sizeC++;
         }
-
     }
-
     conjA = (int*)realloc(conjA, sizeA*sizeof(int));
     conjB = (int*)realloc(conjB, sizeB*sizeof(int));
     conjC = (int*)realloc(conjC, sizeC*sizeof(int));
-
 }
 
 void menuPrincipal(){
 
-    int opc;
+    int opc; 
 
-    do{
-        
+    do{   
+        printf("Conjunto Universal:\n");
+        mostrarConjunto(universo, tamano);
+
         printf("1 - Para ver los conjuntos A,B o C\n");
         printf("2 - Para ver las intersecciones entre A,B o C\n");
         printf("3 - Para ver los complementos de A,B o C\n");
@@ -116,17 +106,27 @@ void menuPrincipal(){
             break;
         case 4:
             system("cls");
-            complementConjRespectoA(conjA, conjB, sizeA, sizeB);
+            complemetAuBuC();
+            system("pause");
+            system("cls");
             break;
         case 5:
             system("cls");
-            complementConjRespectoA(conjA, conjC, sizeA, sizeC);
+            complementConjRespectoA(conjA, conjB, sizeA, sizeB);
+            system("pause");
+            system("cls");
             break;
         case 6:
-            /* Complemento de A en union con B en union con C */
+            system("cls");
+            complementConjRespectoA(conjA, conjC, sizeA, sizeC);
+            system("pause");
+            system("cls");
             break;
         case 7:
-            /* Diferencia simetrica */
+            system("cls");
+            diferenciaSimetrica(conjA, conjB, sizeA, sizeB);
+            system("pause");
+            system("cls");
             break;
         case 8:
             /* FIN */
@@ -136,19 +136,24 @@ void menuPrincipal(){
             printf("\nIngreso una opcion incorrecta\nVuelva a intentar\n");
             break;
         }
-
     } while (opc != 8);
-
 }
 
 void mostrarConjunto(int *conj, int size){
 
-    for(int i = 0; i < size; i++){
-        
-        printf("%i\n", conj[i]);
-        
-    }
+    int i;
 
+    if(size == 0)
+        printf("El conjunto esta vacio\n");
+    else if(size == 1)
+        printf("Conunto: {%i}\n", conj[0]);
+    else{
+        printf("Conjunto: {%i", conj[0]);
+        for(i = 1; i < size; i++){
+            printf(", %i", conj[i]);
+        }
+        printf("}\n");
+    }
 }
 
 void interseEntreDosConjuntos(int *conj1, int *conj2, int size1, int size2){
@@ -157,34 +162,17 @@ void interseEntreDosConjuntos(int *conj1, int *conj2, int size1, int size2){
     interseccion = (int*)malloc((size1 + size2) * sizeof(int));
     int k = 0, i, j;
 
-    if(size1 < 1 || size2 < 1){
-        printf("El conjunto esta vacio\n");
-    }else{
-        if(size1 > size2){
-            for (i = 0; i < size1; i++){
-                for (j = 0; j < size2; j++){
-                    if(conj1[i] == conj2[j]){
-                        interseccion[k] = conj2[j];
-                        k++;
-                    }
-                }
-            }
-        }else{
-            for (i = 0; i < size2; i++){
-                for (j = 0; j < size1; j++){
-                    if(conj2[i] == conj1[j]){
-                        interseccion[k] = conj1[j];
-                        k++;
-                    }
-                }
+    for (i = 0; i < size1; i++){
+        for (j = 0; j < size2; j++){
+            if(conj1[i] == conj2[j]){
+                interseccion[k] = conj2[j];
+                k++;
             }
         }
     }
         
     interseccion = (int*)realloc(interseccion, k*sizeof(int));
-
     mostrarConjunto(interseccion, k);
-
 }
 
 void interseEntrTresConj(){
@@ -218,22 +206,32 @@ void menuConjunto(){
     printf("3 - Conjunto C\n");
     scanf("%i", &opc);
 
-    switch (opc){
-    case 1:
-        printf("\nEl Conjunto A es:\n");
-        mostrarConjunto(conjA, sizeA);
-        break;
-    case 2:
-        printf("\nEl Conjunto B es:\n");
-        mostrarConjunto(conjB, sizeB);
-        break;
-    case 3:
-        printf("\nEl Conjunto C es:\n");
-        mostrarConjunto(conjC, sizeC);
-        break;
-    default:
-        break;
-    }
+    do{
+
+        switch (opc){
+        case 1:
+            system("cls");
+            printf("\nEl Conjunto A es:\n");
+            mostrarConjunto(conjA, sizeA);
+            break;
+        case 2:
+            system("cls");
+            printf("\nEl Conjunto B es:\n");
+            mostrarConjunto(conjB, sizeB);
+            break;
+        case 3:
+            system("cls");
+            printf("\nEl Conjunto C es:\n");
+            mostrarConjunto(conjC, sizeC);
+            break;
+        default:
+            system("cls");
+            printf("Opcion incorrecta, debe elegir entre 1 y 3\nIntente nuevamente\n");
+            break;
+        }
+
+    }while(opc < 1 || opc > 3);
+    
 
     system("pause");
     system("cls");
@@ -251,22 +249,36 @@ void menuInterce(){
     printf("4 - A n B n C\n");
     scanf("%i", &opc);
 
-    switch (opc){
-    case 1:
-        interseEntreDosConjuntos(conjA, conjB, sizeA, sizeB);
-        break;
-    case 2:
-        interseEntreDosConjuntos(conjA, conjC, sizeA, sizeC);
-        break;
-    case 3:
-        interseEntreDosConjuntos(conjB, conjC, sizeB, sizeC);
-        break;
-    case 4:
-        interseEntrTresConj();
-        break;
-    default:
-        break;
-    }
+    do{
+
+        switch (opc){
+        case 1:
+            system("cls");
+            printf("\nEl Conjunto A n B es:\n");
+            interseEntreDosConjuntos(conjA, conjB, sizeA, sizeB);
+            break;
+        case 2:
+            system("cls");
+            printf("\nEl Conjunto A n C es:\n");
+            interseEntreDosConjuntos(conjA, conjC, sizeA, sizeC);
+            break;
+        case 3:
+            system("cls");
+            printf("\nEl Conjunto B n C es:\n");
+            interseEntreDosConjuntos(conjB, conjC, sizeB, sizeC);
+            break;
+        case 4:
+            system("cls");
+            printf("\nEl Conjunto A n B n C es:\n");
+            interseEntrTresConj();
+            break;
+        default:
+            system("cls");
+            printf("Opcion incorrecta, debe elegir entre 1 y 4\nIntente nuevamente\n");
+            break;
+        }
+
+    }while(opc < 1 || opc > 4); 
 
     system("pause");
     system("cls");
@@ -280,22 +292,37 @@ void menuComplementos(){
     printf("Que complementos desea ver?\n");
     printf("1 - Complento de A\n");
     printf("2 - Complento de B\n"); 
-    printf("2 - Complento de C\n");
+    printf("3 - Complento de C\n");
     scanf("%i", &opc);
 
-    switch (opc){
-    case 1:
-        complementoDeConj(conjA, sizeA);
+    do{
+
+        switch (opc){
+        case 1:
+            system("cls");
+            printf("\nEl Complemento de A es:\n");
+            complementoDeConj(conjA, sizeA);
+            break;
+        case 2:
+            system("cls");
+            printf("\nEl Complemento de B es:\n");
+            complementoDeConj(conjB, sizeB);
+            break;
+        case 3:
+            system("cls");
+            printf("\nEl Complemento de C es:\n");
+            complementoDeConj(conjC, sizeC);
+            break; 
+        default:
+            system("cls");
+            printf("Opcion incorrecta, debe elegir entre 1 y 3\nIntente nuevamente\n");
         break;
-    case 2:
-        complementoDeConj(conjB, sizeB);
-        break;
-    case 3:
-        complementoDeConj(conjC, sizeC);
-        break; 
-    default:
-    break;
-   }
+        }
+
+    }while(opc < 1 || opc > 3);
+
+    system("pause");
+    system("cls");
 
 }
 
@@ -347,4 +374,85 @@ void complementConjRespectoA(int *conj1, int *conj2, int size1, int size2){
 
     mostrarConjunto(complementRes, k);
 
+}
+
+void diferenciaSimetrica(int *conj1, int *conj2, int size1, int size2){
+    
+    int *difSimetric;
+    difSimetric = (int*)malloc((size1 + size2) * sizeof(int));
+    int i, j, k = 0;
+    int encontrado = 0;
+
+    for (i = 0; i < size1; i++){
+        for(j = 0; j < size2; j++){\
+            if(conj1[i] == conj2[j])
+                encontrado = 1;
+        } 
+        if(!encontrado){
+            difSimetric[k] = conj1[i];
+            k++;
+        }
+        encontrado = 0;
+    }
+
+    for (i = 0; i < size2; i++){
+        for(j = 0; j < size1; j++){\
+            if(conj2[i] == conj1[j])
+                encontrado = 1;
+        } 
+        if(!encontrado){
+            difSimetric[k] = conj2[i];
+            k++;
+        }
+        encontrado = 0;
+    }
+
+    difSimetric = (int*)realloc(difSimetric, k*sizeof(int));
+
+    mostrarConjunto(difSimetric, k);
+
+}
+
+void complemetAuBuC(){
+    int *complementoAuBuC;
+    complementoAuBuC = (int*)malloc(tamano * sizeof(int));
+    int k = 0;
+    int i, j;
+    int encontrado = 0;
+
+    for (i = 0; i < tamano; i++){
+        for(j = 0; j < sizeA; j++){\
+            if(universo[i] == conjA[j])
+                encontrado = 1;
+        } 
+        if(!encontrado){
+            complementoAuBuC[k] = universo[i];
+            k++;
+        }
+        encontrado = 0;
+    }
+    for (i = 0; i < sizeB; i++){
+        for(j = 0; j < tamano; j++){\
+            if(conjB[i] == complementoAuBuC[j])
+                encontrado = 1;
+        } 
+        if(!encontrado){
+            complementoAuBuC[k] = conjB[i];
+            k++;
+        }
+        encontrado = 0;
+    }
+    for (i = 0; i < sizeC; i++){
+        for(j = 0; j < tamano; j++){\
+            if(conjC[i] == complementoAuBuC[j])
+                encontrado = 1;
+        } 
+        if(!encontrado){
+            complementoAuBuC[k] = conjC[i];
+            k++;
+        }
+        encontrado = 0;
+    }
+    complementoAuBuC = (int*)realloc(complementoAuBuC, k*sizeof(int));  
+    mostrarConjunto(complementoAuBuC, k);        
 }
